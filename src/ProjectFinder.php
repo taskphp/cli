f1b25e7759451fea82b7b100b773fe60fccd0ec5
Lauris Binde <laurisb@users.noreply.github.com>
2014-05-30 08:46:14 +0300

    PR for taskphp/task#6

--- src/ProjectFinder.php
+++ src/ProjectFinder.php
@@ -6,21 +6,58 @@ use Task\Project;
 
 class ProjectFinder
 {
-    public function find($taskfile = './Taskfile')
+    /**
+     * Taskfile basename variants, in priority order.
+     * 
+     * @var array
+     */
+    private $variants = ['Taskfile', 'taskfile', 'taskfile.php'];
+    
+    /**
+     * @return string|null
+     */
+    public function findTaskfile()
     {
-        if (!file_exists($taskfile)) {
+        $taskfile = null;
+        
+        $cwd = getcwd();
+        
+        foreach($this->variants as $variant)
+        {
+            $file = $cwd . DIRECTORY_SEPARATOR . $variant;
+            
+            if(is_file($file))
+            {
+                $taskfile = $file;
+                
+                break;
+            }
+        }
+        
+        return $taskfile;
+    }
+    
+    public function find()
+    {
+        $taskfile = $this->findTaskfile();
+        
+        // FIXME: This can go in findTaskfile() and then we can get rid of returning NULL.
+        if($taskfile === null)
+        {
             throw new \InvalidArgumentException("$taskfile not found");
         }
 
-        if (filesize($taskfile) === 0) {
+        // FIXME: This is redundant, because return check below will handle this.
+        if(filesize($taskfile) === 0)
+        {
             throw new \LogicException("Taskfile is empty");
         }
 
         $project = require $taskfile;
-
-        $cls = 'Task\Project';
-        if (!($project instanceof $cls)) {
-            throw new \LogicException("Taskfile must return a Project");
+        
+        if(!($project instanceof '\Task\Project'))
+        {
+            throw new \LogicException("Taskfile must return an instance of '\Task\Project'");
         }
 
         return $project;
